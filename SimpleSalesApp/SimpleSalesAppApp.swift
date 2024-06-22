3f8a236769eb224609f30924d4cb87be6a5952a0
Fahmi Dzulqarnain <fdz.fhz1302@gmail.com>
2024-06-22 14:58:14 +0800

    Initial Commit

---
+++ SimpleSalesApp/SimpleSalesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSalesAppApp.swift
+//  SimpleSalesApp
+//
+//  Created by Fahmi Dzulqarnain on 22/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSalesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
